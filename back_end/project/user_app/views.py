696d67df93cdd04c6cf7df0f73b862cf440cb1e8
Vivek-raj-gupta-2002 <vivekrajgupta2002@outlook.com>
2023-12-13 21:59:46 +0530

    added Dashboard with almost all functions

--- back_end/project/user_app/views.py
+++ back_end/project/user_app/views.py
@@ -2,15 +2,20 @@
 from django.shortcuts import render, redirect
 
 from main_app.models import OneTimePass, CustomUser, College
+from . import models
 from . import forms
 
 from django.views.decorators.http import require_http_methods
 from utills import mail, dummy_data, valid_otp
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from random import randint
 
 from django.contrib.auth import authenticate, login, logout
 
+
+links = {
+    'scholar': 'https://cdn.s3waas.gov.in/s31385974ed5904a438616ff7bdb3f7439/uploads/2019/06/2019062152.jpg'
+}
 
 # Create your views here.
 @require_http_methods(["GET", "POST"])
@@ -29,11 +34,93 @@ def logout_view(request):
     
     return redirect('login')
 
+# the dashboard
 
+# to show up all the data
+@require_http_methods(["GET"])
 def dashboard_view(request):
-    return HttpResponse("Dashboard")
+    
+    #if user is not authenticated
+    if not(request.user.is_authenticated):
+
+        # redirecte the user to login page
+        return redirect('login')
+    
+    # now if user is authenticated
+    my_user = CustomUser.objects.filter(username = request.user.username).first()
+
+    if not my_user:
+        # if user not found
+        return Http404()
+
+    #if not student
+    
+    if not(my_user.is_student):
+        return HttpResponse("institute user")
+    
+    # the correct user:
+
+    # aadhar data and marks data
+
+    marks = dummy_data.get_marksheet(aadhar_no=my_user.aadhar_number)
+    
+    data = dummy_data.get_aadhar(aadhar_no=my_user.aadhar_number)
+
+    
+    send_data = {}
+
+    # alligned them correctly
+    
+    if data:
+        send_data['aadhar']: data
+    
+    for i in marks:
+        if i.standard == '1':
+            send_data['10'] = i
+        else:
+            send_data['12'] = i
+
+    caste = dummy_data.get_caste(my_user.aadhar_number)
+    domacile = dummy_data.get_domicile(my_user.aadhar_number)
+    income = dummy_data.get_income(my_user.aadhar_number)
+
+
+    send_data['scolar'] = {}
+
+    if caste:
+        send_data['scolar']['caste'] = caste
+
+    if domacile:
+        send_data['scolar']['domacile'] = domacile
+    if income:
+        send_data['scolar']['income'] = income
+
+
+    upload_docs = models.UploadForm.objects.filter(user=my_user)
+
+    send_data['edu'] = []
+    send_data['sco'] = []
+
+    for i in upload_docs:
+        if i.document_type in ('10th Marksheet', '12th Marksheet', 'Migration', 'Trancerfer Certificate', 'Marksheet', 'GAP Certificate', 'Admission Slip'):
+            send_data['edu'].append(i)
+            continue
+
+        if i.document_type in ('Domacile', 'Income', 'Caste', 'Samagra', 'Bank Passbook'):
+            send_data['sco'].append(i)
+            continue
+
+    
+
+    send_data['upload'] = upload_docs
+    
+    return render(request, 'user_app/eziiii.html', send_data)
+
+
+
 
 # authenticate user 
+@require_http_methods(["GET", 'POST'])
 def login_view(request):
     # if user is already authenticated then
     # redirect to dashboard ------------------------------------------>
@@ -87,6 +174,7 @@ def login_view(request):
     return render(request, 'user_app/log-in.html', {'form': my_form})
 
 # creating user
+@require_http_methods(["GET", "POST"])
 def signup(request):
     # if user is already authenticated then
     # redirect to dashboard ------------------------------------------>
