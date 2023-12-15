ed4fbfea7cd31a9de893588a9c2774e4e41ead74
Sanskargoyal608 <sanskargoyal608@gmail.com>
2023-12-16 01:29:20 +0530

    Upload Document Button Activated

--- back_end/project/utills/college.py
+++ back_end/project/utills/college.py
@@ -2,17 +2,20 @@ import requests
 
 url = "https://aishe.gov.in/aishe/instDirectoryCollegeIndex"
 
-data = {
-    "flag": False,
-    "stateId": -1,
-    "districtCode": -1,
-    "universitytypeId": -1,
-    "universityId": -1,
-    "collegeType": -1,
-    "reportFormatType": 3
+payload = {'flag': 'false',
+'stateId': '-1',
+'districtCode': '-1',
+'universitytypeId': '-1',
+'universityId': '-1',
+'collegeType': '-1',
+'reportFormatType': '3'}
+files=[
+
+]
+headers = {
+  'Cookie': 'JSESSIONID=9DDFC3A3B507551EB6233317AB507F8F; SERVERID=aishe5'
 }
 
-response = requests.post(url, data=data)
+response = requests.request("POST", url, headers=headers, data=payload, files=files, timeout=10)
 
-print(response.status_code)
 print(response.text)
